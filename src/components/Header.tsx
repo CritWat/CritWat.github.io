4a30456c9eb3dd0fecd9e5e5b2580050877d6c77
Chris <rewindyourtape@gmail.com>
2025-01-27 15:46:46 -0700

    wip router refactor

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,161 @@
 import { NavLink } from "react-router-dom";
+import {useDisclosure} from "@mantine/hooks";
+import {MdUpload, MdViewInAr} from "react-icons/md";
+import {
+    Box, Burger,
+    Button,
+    Center, Collapse, Divider, Drawer,
+    Group,
+    HoverCard,
+    ScrollArea,
+    SimpleGrid,
+    Text,
+    ThemeIcon,
+    UnstyledButton
+} from "@mantine/core";
+import classes from "./Header.module.css";
+import {FaChevronDown} from "react-icons/fa";
+import {IoMdTrophy} from "react-icons/io";
+
+const modelLinks = [
+    {
+        icon: MdViewInAr,
+        title: "Model Viewer",
+        description: "View models in 3D",
+        link: "/",
+    },
+    {
+        icon: MdUpload,
+        title: "Upload Model",
+        description: "Upload your own model",
+        link: "/",
+    },
+    {
+        icon: IoMdTrophy,
+        title: "My Designs",
+        description: "A collection of some of my blender projects used for 3D printing",
+        link: "/models"
+    },
+]
 
 export const Header = () => {
+    const [drawerOpened, {toggle: toggleDrawer, close: closeDrawer}] = useDisclosure(false);
+    const [linksOpened, { toggle: toggleLinks }] = useDisclosure(false);
+
+    const links = modelLinks.map((link) => (
+        <NavLink to={link.link}>
+            <UnstyledButton className={classes.subLink} key={link.title}>
+
+                <Group wrap="nowrap" align={"center"}>
+
+                    <ThemeIcon size={34} variant={"default"} radius={"md"}>
+                        <link.icon size={22} color={"#fff"}/>
+                    </ThemeIcon>
+
+                    <div>
+                        <Text size="sm" fw={500}>
+                            {link.title}
+                        </Text>
+                        <Text size="xs" c="dimmed">
+                            {link.description}
+                        </Text>
+                    </div>
+                </Group>
+            </UnstyledButton>
+        </NavLink>
+    ))
+
     return(
-        <nav>
-            <ul>
-                <li>
-                    <NavLink to="/">Home</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/models">Model Viewer</NavLink>
-                </li>
-            </ul>
-        </nav>
+        <Box pb={120}>
+            <header className={classes.header}>
+                <Group justify={"right"} h="100%">
+                    <Group h="100%" gap={0} visibleFrom="sm">
+                        <NavLink to={"/"} className={classes.link}>
+                            Home
+                        </NavLink>
+                        <HoverCard width={600} position="bottom" radius="md" shadow="md" withinPortal>
+                            <HoverCard.Target>
+                                <a href="#" className={classes.link}>
+                                    <Center inline>
+                                        <Box component="span" mr={5}>
+                                            3D Models
+                                        </Box>
+                                        <FaChevronDown size={16} />
+                                    </Center>
+                                </a>
+                            </HoverCard.Target>
+
+                            <HoverCard.Dropdown style={{ overflow: 'hidden' }}>
+                                <Group justify="space-between" px="md">
+                                    <Text fw={500}>3D Models</Text>
+                                </Group>
+
+                                <Divider my="sm" />
+
+                                <SimpleGrid cols={2} spacing={0}>
+                                    {links}
+                                </SimpleGrid>
+
+                                <div className={classes.dropdownFooter}>
+                                    <Group justify="space-between">
+                                        <div>
+                                        </div>
+                                    </Group>
+                                </div>
+                            </HoverCard.Dropdown>
+                        </HoverCard>
+                        <NavLink to="/about" className={classes.link}>
+                            About
+                        </NavLink>
+                    </Group>
+
+                    <Divider orientation="vertical"/>
+
+                    <Group visibleFrom="sm">
+                        <Button variant="default">Log in</Button>
+                        <Button>Sign up</Button>
+                    </Group>
+
+                    <Burger opened={drawerOpened} onClick={toggleDrawer} hiddenFrom="sm" />
+                </Group>
+            </header>
+
+            <Drawer
+                opened={drawerOpened}
+                onClose={closeDrawer}
+                size="100%"
+                padding="md"
+                title="Navigation"
+                hiddenFrom="sm"
+                zIndex={1000000}
+            >
+                <ScrollArea h="calc(100vh - 80px" mx="-md">
+
+                    <a href="#" className={classes.link}>
+                        Home
+                    </a>
+                    <UnstyledButton className={classes.link} onClick={toggleLinks}>
+                        <Center inline>
+                            <Box component="span" mr={5}>
+                                Features
+                            </Box>
+                            <FaChevronDown size={16} />
+                        </Center>
+                    </UnstyledButton>
+                    <Collapse in={linksOpened}>{links}</Collapse>
+                    <a href="#" className={classes.link}>
+                        Learn
+                    </a>
+                    <a href="#" className={classes.link}>
+                        Academy
+                    </a>
+
+                    <Group justify="center" grow pb="xl" px="md">
+                        <Button variant="default">Log in</Button>
+                        <Button>Sign up</Button>
+                    </Group>
+                </ScrollArea>
+            </Drawer>
+        </Box>
     )
 }
--- src/views/Home.page.tsx
+++ src/views/Home.page.tsx
@@ -1,9 +1,11 @@
 import {Welcome} from "../Welcome/Welcome.tsx";
+import {ColorSchemeToggle} from "../components/ColorSchemeToggle/ColorSchemeToggle.tsx";
 
 export function HomePage() {
     return (
         <>
             <Welcome />
+            <ColorSchemeToggle />
         </>
     );
 }
